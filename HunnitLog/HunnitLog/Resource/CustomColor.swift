695498b782f4a4ff3c1256c5e0991504fce011b9
iamcho2 <thatgrass@naver.com>
2020-11-22 19:54:48 +0900

    [NON_ISSUE] 컬러 에셋 추가
    ## Description
    컬러 에셋 추가했습니다
    `CustomColor.lightGray`로 사용하시면 됩니다!

---
+++ HunnitLog/HunnitLog/Resource/CustomColor.swift
@@ -0,0 +1,19 @@
+//
+//  CustomColor.swift
+//  HunnitLog
+//
+//  Created by 초이 on 2020/11/22.
+//
+
+import SwiftUI
+
+struct CustomColor {
+    static let bgColor = Color("bgColor")
+    static let gray = Color("Grey")
+    static let darkGray = Color("darkGrey")
+    static let coolGray = Color("coolGrey")
+    static let lightGray = Color("lightGrey")
+    static let yellow = Color("yellow")
+    static let black = Color("black")
+}
+
