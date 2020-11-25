4c3ba9bd8695a8667522d7be74a002da4fe97e76
iamcho2 <thatgrass@naver.com>
2020-11-26 02:00:35 +0900

    [NON_ISSUE] 홈 top 뷰 작성

---
+++ HunnitLog/HunnitLog/View/Home/HomeView.swift
@@ -0,0 +1,32 @@
+//
+//  HomeView.swift
+//  HunnitLog
+//
+//  Created by 초이 on 2020/11/26.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    var body: some View {
+        ZStack{
+            CustomColor.bgColor
+            VStack{
+                ZStack{
+                    CustomColor.bgColor
+                    RoundedBackground()
+                    ProgressBar()
+                }
+                .frame(width: UIScreen.main.bounds.width, height: 250)
+                .edgesIgnoringSafeArea(.top)
+            }
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
