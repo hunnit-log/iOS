810159af296ad21953c95d4081c8032a34c29198
iamcho2 <thatgrass@naver.com>
2020-12-05 16:51:48 +0900

    [#22] 점수 입력 Row 작성

---
+++ HunnitLog/HunnitLog/View/Home/ScoreRow.swift
@@ -0,0 +1,20 @@
+//
+//  ScoreRow.swift
+//  HunnitLog
+//
+//  Created by 초이 on 2020/12/05.
+//
+
+import SwiftUI
+
+struct ScoreRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ScoreRow_Previews: PreviewProvider {
+    static var previews: some View {
+        ScoreRow()
+    }
+}
